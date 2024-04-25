79f5fe183c47f1242c185b5c9c91fd3947a2753a
Saurabh <saurabh.chaudhary@mooo.farm>
2024-04-26 00:35:19 +0530

    added backend code

---
+++ backend/models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    picture: {
+      type: String,
+      required: true,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { chats } = require("./data/data");
 const dotenv = require("dotenv");
-
+const connectDB = require("./config/db");
+const colors = require("colors");
 const app = express();
 dotenv.config();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send({
@@ -11,16 +14,11 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-  console.log(req.params.id);
-});
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, console.log(`Server is started on the PORT ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server is started on the PORT ${PORT}`.yellow.bold)
+);
